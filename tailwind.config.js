f230fc2a010f6ad26d790862d724b230cb06bfdb
t4n1shq <127678797+t4n1shq@users.noreply.github.com>
2025-07-12 18:23:53 +0530

    change1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
+const { rgbaColor } = require('react-native-reanimated/lib/typescript/Colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-    content: ["./app/**/*.{js,jsx,ts,tsx"],
+    content: ["./app/**/*.{js,jsx,ts,tsx}"],
     presets: [require("nativewind/preset")],
     theme: {
-    extend: {},
+    extend: {
+      primary: '#030014',
+    },
     },
     plugins: [],
 }
